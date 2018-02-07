447a8cc8a7b64e88b15acf915cb3a81446f26e8d
SUE <sue.gutierrez@opendeusto.es>
2018-02-07 18:02:37 +0100

    Primera versión Prueba

---
+++ test.R
@@ -0,0 +1,13 @@
+# Este es un script R de prueba para github
+# Pruebas
+# SUE
+
+
+####################################################################
+# Configurando el entorno de trabajo
+####################################################################
+rm(list=ls())
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+dir()
+
